1cb13924b3b539d2dbf5aff6fadd4e4a326e47a9
Aaron Collins <aaron777collins@gmail.com>
2025-08-30 14:36:21 -0400

    Fix I/O test expectations to match actual output after load.mcfunction bug fix

--- tests/test_io.py
+++ tests/test_io.py
@@ -261,7 +261,13 @@ scoreboard objectives add counter dummy
 scoreboard players set @e[type=armor_stand,tag=mdl_server,limit=1] counter 0''',
         
         # Main function with while loop (no armor stand setup needed)
-        "data/test/function/main.mcfunction": '''''',
+        "data/test/function/main.mcfunction": '''execute unless entity @e[type=armor_stand,tag=mdl_server,limit=1] run summon armor_stand ~ 320 ~ {Tags:["mdl_server"],Invisible:1b,Marker:1b,NoGravity:1b,Invulnerable:1b}
+execute if score @e[type=armor_stand,tag=mdl_server,limit=1] counter matches ..2 run function test:test_main_while_0''',
+        
+        # While loop body function
+        "data/test/function/test_main_while_0.mcfunction": '''tellraw @a [{"text":"Counter: "},{"score":{"name":"@e[type=armor_stand,tag=mdl_server,limit=1]","objective":"counter"}}]
+scoreboard players add @e[type=armor_stand,tag=mdl_server,limit=1] counter 1
+execute if score @e[type=armor_stand,tag=mdl_server,limit=1] counter matches ..2 run function test:test_main_while_0''',
         
         # Load tag
         "data/minecraft/tags/function/load.json": '''{
